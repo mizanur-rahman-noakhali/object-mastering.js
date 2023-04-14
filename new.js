0d9ecd63f388923b4cdab6b931536a7dc3e7aab5
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-04-14 19:24:55 +0600

    class-object

---
+++ new.js
@@ -0,0 +1,12 @@
+class person{
+constructor(firstName,lastName,salary){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.salary = salary;
+}
+
+}
+const heroPerson=new person('hero','alom',20000);
+console.log(heroPerson);
+const friendPerson=new person('abir','khan',25000);
+console.log(friendPerson);
