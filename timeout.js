6ba41d8b598d40ef6017f407ad0ac3e3370f7c99
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-04-15 15:56:40 +0600

    set time-set-interval

---
+++ timeout.js
@@ -0,0 +1,24 @@
+function doSomething(){
+  console.log(3333);
+}
+
+
+console.log(2222);
+//setTimeout(doSomething,4000);
+
+// setTimeout(function(){
+// console.log('lazy person');
+// },4000);
+
+// setTimeout(()=> {
+//   console.log("see you latter");
+// },4000);
+
+setInterval(function()  {
+  console.log("do something");
+}, 3000);
+console.log(4444);
+console.log(5555);
+console.log(6666);
+console.log(7777);
+console.log(8888);
